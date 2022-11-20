3aa933c20ce3ade501c11891e796816764db9780
Bakk <ion-menghini@hotmail.it>
2022-11-20 11:41:27 +0100

    implementati Test classe Account

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,35 +1,76 @@
 package test;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import code.Businnes_Logic.Account;
+import code.Businnes_Logic.Euro;
 
 public class AccountTest {
-    @Test
-    void testCredit() {
 
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000, -10})
+    void testCredit(int amount) {
+        Account accountBase = new Account(1,1, new Euro(0), new Euro(0));
+        accountBase.credit(new Euro(amount));
+        var amountInCent = amount*100;
+        assertEquals(amountInCent, accountBase.getTotalBalance().getValore());
     }
 
-    @Test
-    void testDebit() {
-
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000, -10})
+    void testDebitAvailableBalance(int amount) {
+        Account accountBase = new Account(1,1, new Euro(amount), new Euro(0));
+        accountBase.debit(new Euro(amount));
+        assertEquals(0, accountBase.getAvailableBalance().getValore());
     }
 
-    @Test
-    void testGetAccountNumber() {
-
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000, -10})
+    void testDebitTotalBalance(int amount) {
+        Account accountBase = new Account(1,1, new Euro(0), new Euro(amount));
+        accountBase.debit(new Euro(amount));
+        assertEquals(0, accountBase.getTotalBalance().getValore());
     }
 
-    @Test
-    void testGetAvailableBalance() {
-
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000})
+    void testGetAccountNumber(int accNumber) {
+        Account accountBase = new Account(accNumber,1, new Euro(0), new Euro(0));
+        assertEquals(accNumber, accountBase.getAccountNumber());
     }
 
-    @Test
-    void testGetTotalBalance() {
-
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000})
+    void testGetAvailableBalance(int amount) {
+        Account accountBase = new Account(1,1, new Euro(amount), new Euro(0));
+        var amountInCent = amount*100;
+        assertEquals(amountInCent, accountBase.getAvailableBalance().getValore());
     }
 
-    @Test
-    void testValidatePIN() {
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 10, 100, 1000})
+    void testGetTotalBalance(int amount) {
+        Account accountBase = new Account(1,1, new Euro(0), new Euro(amount));
+        var amountInCent = amount*100;
+        assertEquals(amountInCent, accountBase.getTotalBalance().getValore());
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = { 123087, 1123550, 189670})
+    void testValidatePIN(int pin) {
+        Account accountBase = new Account(1, pin, new Euro(0), new Euro(0));
+        assertTrue(accountBase.validatePIN(pin));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 123087, 1123550, 189670})
+    void testFailValidatePIN(int wrongPin) {
+        Account accountBase = new Account(1, 00000, new Euro(0), new Euro(0));
+        assertFalse(accountBase.validatePIN(wrongPin));
     }
 }
